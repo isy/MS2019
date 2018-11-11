ac9caf10a4cd333b4a0ce339f203d570a8e8e970
Hironori Akaishi <ah.gm3622@gmail.com>
2018-11-11 19:06:51 +0900

    add: RayController, EnemyController

---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour
+{
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit = new RaycastHit();
+      if (Physics.Raycast(ray, out hit))
+      {
+        if (hit.transform.gameObject.CompareTag("enemy"))
+        {
+          EnemyHit(hit);
+        }
+      }
+    }
+  }
+
+  private void EnemyHit(RaycastHit hit)
+  {
+    hit.transform.gameObject.GetComponent<EnemyController>().Hit();
+  }
+}
