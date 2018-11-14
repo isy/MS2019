3fe358db3de6901d922e9758468599cb11d12b2d
Hironori Akaishi <ah.gm3622@gmail.com>
2018-11-15 02:20:08 +0900

    add: title & UniRx

---
+++ Assets/Scripts/Menu/FlashingText.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class FlashingText : MonoBehaviour
+{
+  [SerializeField]
+  private float angularFrequency = 5f;
+  static readonly float DeltaTime = 0.0333f;
+  private Text text;
+  // Use this for initialization
+  void Start()
+  {
+    float time = 0.0f;
+    text = GetComponent<Text>();
+    Observable.Interval(TimeSpan.FromSeconds(DeltaTime)).Subscribe(_ =>
+      {
+        time += angularFrequency * DeltaTime;
+        var color = text.color;
+        color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+        text.color = color;
+      }).AddTo(this);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
