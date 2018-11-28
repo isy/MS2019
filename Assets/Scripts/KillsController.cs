5f8c3563a9ed00b64e211bfe8958a75d93db5f10
Hironori Akaishi <ah.gm3622@gmail.com>
2018-11-28 10:50:38 +0900

    add: count kills

---
+++ Assets/Scripts/KillsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KillsController : MonoBehaviour
+{
+
+  [SerializeField]
+  public int killsCount = 0;
+  private Text killText;
+
+  void Awake()
+  {
+    killText = this.gameObject.GetComponent<Text>();
+    killText.text = killsCount.ToString();
+  }
+
+  public void incrementCounter()
+  {
+    killsCount++;
+    killText.text = killsCount.ToString();
+    if (killsCount >= 10)
+    {
+      SceneManager.LoadSceneAsync("Score");
+    }
+
+  }
+}
